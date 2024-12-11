decff1d0b1c2c1f74e9083d730d624f7993ea024
SlipkHunter <abrito025@gmail.com>
2024-12-11 10:33:53 -0300

    Small improvements

--- extension.js
+++ extension.js
@@ -3,13 +3,14 @@ const Main = imports.ui.main;
 // did we activate the overview?
 let _active = false;
 
-function check_with_delay() {
-  // when switching too fast it causes crash in animations
-  setTimeout(check, 100);
+function hasWindowsActive() {
+  return global.workspace_manager.get_active_workspace().list_windows().length
 }
 
-function check() {
-  if (!global.workspace_manager.get_active_workspace().list_windows().length) {
+function check_workspace_switched() {
+  // when switching too fast it causes crash in animations
+  setTimeout(function() {
+    if (!hasWindowsActive()) {
     // workspace empty
     if (!Main.overview.visible) {
       _active = true;
@@ -21,17 +22,33 @@ function check() {
       _active = false;
       Main.overview.hide();
     }
+    }
+  }, 50);
+}
+
+function check_window_created() {
+  if (Main.overview.visible && hasWindowsActive()) { // overview visible and workspace not empty
+    Main.overview.hide();
+  }
+}
+
+function check_window_destroyed() {
+  if (!Main.overview.visible && !hasWindowsActive()) { // overview not visible and workspace empty
+    _active = true;
+    Main.overview.show();
   }
 }
 
 let _signalIds = [];
 
 function enable() {
-  _signalIds[0] = global.workspace_manager.connect('workspace-switched', check_with_delay);
-  _signalIds[1] = global.display.connect('restacked', check_with_delay);
+  _signalIds[0] = global.workspace_manager.connect('workspace-switched', check_workspace_switched);
+  _signalIds[1] = global.window_manager.connect('destroy', check_window_destroyed);
+  _signalIds[2] = global.display.connect('window-created', check_window_created);
 }
 
 function disable() {
   global.workspace_manager.disconnect(_signalIds[0]);
-  global.display.disconnect(_signalIds[1]);
+  global.window_manager.disconnect(_signalIds[1]);
+  global.display.disconnect(_signalIds[2]);
 }
